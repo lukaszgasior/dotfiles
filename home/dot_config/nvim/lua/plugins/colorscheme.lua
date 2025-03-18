1b08901fd5e5cab102c17379b8fbd3ea1b1661af
Lukasz Gasior <lukasz@gasior.net.pl>
2025-03-18 21:15:03 +0100

    nvim

---
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "ellisonleao/gruvbox.nvim", config = {
+    transparent_mode = true,
+  } },
+  {
+    "lazyvim/lazyvim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
