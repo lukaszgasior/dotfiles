a381575db9381c3db11955b9a5a48c6054e13764
Lukasz Gasior <lukasz@gasior.net.pl>
2025-10-01 14:04:27 +0200

    updated powershell profile and prompt

--- home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,24 @@
-if ([Environment]::UserInteractive) {
+if ([Environment]::UserInteractive)
+{
 
-   if ($PSStyle) {
+   if ($PSStyle)
+   {
       $PSStyle.FileInfo.Directory = "`e[38;2;255;255;255m"
       $PSStyle.FileInfo.Executable = "`e[38;2;255;255;255m"
       $PSStyle.FileInfo.SymbolicLink = "`e[38;2;255;255;255m"
       $PSStyle.FileInfo.Extension.Clear()
-      $PSStyle.Formatting.TableHeader = ""
-      $PSStyle.Formatting.FormatAccent = ""
+      $PSStyle.Formatting.TableHeader = ''
+      $PSStyle.Formatting.FormatAccent = ''
       $PSStyle.Progress.View = 'Classic'
    }
 
-   if (Get-Command starship -ErrorAction SilentlyContinue) {
+   if (Get-Command starship -ErrorAction SilentlyContinue)
+   {
       Invoke-Expression (&starship init powershell)
    }
 
-   if (Get-Module -ListAvailable PSReadLine) {
+   if (Get-Module -ListAvailable PSReadLine)
+   {
       Set-PSReadLineOption -PredictionSource History
       Set-PSReadLineOption -PredictionViewStyle ListView
       Set-PSReadLineOption -HistorySearchCursorMovesToEnd
@@ -32,14 +36,24 @@ if ([Environment]::UserInteractive) {
       }
 
       Set-PSReadLineOption -Colors @{
-         Command   = 'Cyan'
-         Parameter = 'DarkCyan'
-         String    = 'Green'
-         Operator  = 'DarkGray'
+         Command          = '#b8bb26'
+         Parameter        = '#83a598'
+         String           = '#d79921'
+         Operator         = '#fe8019'
+         Variable         = '#8ec07c'
+         Number           = '#d3869b'
+         Member           = '#fabd2f'
+         Type             = '#d65d0e'
+         Comment          = '#928374'
+         Keyword          = '#fb4934'
+         Error            = '#cc241d'
+         Selection        = '#504945'
+         InlinePrediction = '#665c54'
       }
    }
 
-   if (Get-Command kubectl -ErrorAction SilentlyContinue) {
+   if (Get-Command kubectl -ErrorAction SilentlyContinue)
+   {
       kubectl completion powershell | Out-String | Invoke-Expression
 
       Set-Alias -Name k -Value kubectl
@@ -47,7 +61,8 @@ if ([Environment]::UserInteractive) {
    }
 }
 
-if (Get-Command nvim -ErrorAction SilentlyContinue) {
+if (Get-Command nvim -ErrorAction SilentlyContinue)
+{
    $env:EDITOR = 'nvim'
    $env:VISUAL = 'nvim'
 
@@ -55,23 +70,39 @@ if (Get-Command nvim -ErrorAction SilentlyContinue) {
    Set-Alias -Name vi -Value nvim
 }
 
-function dotfiles { Set-Location "C:\tools\dotfiles\" }
-
-function take {
-   [CmdletBinding()]
+function Find-AndNavigate
+{
    param(
       [Parameter(Mandatory = $true)]
-      $Path
+      [string]$RelativePath
    )
-   New-Item -Path $Path -ItemType Directory | Out-Null
-   Set-Location -Path $Path
+
+   $drives = Get-PSDrive -PSProvider FileSystem | Where-Object { $_.Root -match '^[A-Z]:\\$' }
+
+   foreach ($drive in $drives)
+   {
+      $fullPath = Join-Path $drive.Root $RelativePath
+      if (Test-Path $fullPath)
+      {
+         Set-Location $fullPath
+         return
+      }
+   }
+
+   Write-Warning "Can't find [$RelativePath]"
 }
 
-function which ($command) {
+function projects { Find-AndNavigate 'projects' }
+function github { Find-AndNavigate 'github' }
+function take($Path) { mkdir $Path | Out-Null; Set-Location $Path }
+
+function which ($command)
+{
    Get-Command -Name $command -ErrorAction SilentlyContinue |
    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
-function Get-PublicIP {
+function Get-PublicIP
+{
    (Invoke-RestMethod -Uri 'https://api.ipify.org?format=json').ip
 }
