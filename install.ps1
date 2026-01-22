701be40112b0abb919637daa23b42a65546fd2d1
Lukasz Gasior <lukasz@gasior.net.pl>
2026-01-22 09:13:49 +0100

    winget update

--- install.ps1
+++ install.ps1
@@ -1,21 +1,23 @@
 # Install prerequisites and dotfiles
 
 # Check if Git is installed
-if (!(Get-Command git -ErrorAction SilentlyContinue)) {
-    Write-Host "Installing Git..."
-    winget install -e --id Git.Git -h --accept-source-agreements --accept-package-agreements
+if (!(Get-Command git -ErrorAction SilentlyContinue))
+{
+    Write-Host 'Installing Git...'
+    winget install -e --id Git.Git -h --accept-source-agreements --accept-package-agreements --source winget
     # Refresh environment variables
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+    $env:Path = [System.Environment]::GetEnvironmentVariable('Path', 'Machine') + ';' + [System.Environment]::GetEnvironmentVariable('Path', 'User')
 }
 
 # Check if chezmoi is installed
-if (!(Get-Command chezmoi -ErrorAction SilentlyContinue)) {
-    Write-Host "Installing chezmoi..."
-    winget install -e --id twpayne.chezmoi --accept-source-agreements --accept-package-agreements
+if (!(Get-Command chezmoi -ErrorAction SilentlyContinue))
+{
+    Write-Host 'Installing chezmoi...'
+    winget install -e --id twpayne.chezmoi --accept-source-agreements --accept-package-agreements --source winget
     # Refresh environment variables
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+    $env:Path = [System.Environment]::GetEnvironmentVariable('Path', 'Machine') + ';' + [System.Environment]::GetEnvironmentVariable('Path', 'User')
 }
 
 # Initialize and apply dotfiles
-Write-Host "Initializing dotfiles..."
+Write-Host 'Initializing dotfiles...'
 chezmoi init --apply lukaszgasior/dotfiles
